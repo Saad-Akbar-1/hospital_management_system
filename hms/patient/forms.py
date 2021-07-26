90429a876513473dd032f4126d6379b8b356adaf
Saad Akbar <saad.akbar@arbisoft.com>
2021-07-26 13:50:10 +0500

    Added valdiation checks form for email and Patient name

--- hms/patient/forms.py
+++ hms/patient/forms.py
@@ -3,6 +3,7 @@ The signup form design for Patient Model.
 '''
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import EmailValidator, RegexValidator
 from phonenumber_field.modelfields import PhoneNumberField
 
 from patient.models import Patient
@@ -10,8 +11,9 @@ from patient.models import Patient
 
 class SignUpForm(UserCreationForm):
     '''The custom signup form class for Patient Model'''
-    patient_name = forms.CharField(max_length=30)
-    patient_email = forms.EmailField(max_length=200)
+    alphabets = RegexValidator(r'^[a-zA-Z]*$', 'Only alphabets are allowed.')
+    patient_name = forms.CharField(max_length=30,validators=[alphabets])
+    patient_email = forms.EmailField(max_length=200,validators=[EmailValidator])
     patient_contact = PhoneNumberField(max_length=15)
     birth_date = forms.DateField(help_text='Required. Format: YYYY-MM-DD')
     GENDER_CHOICES = (
